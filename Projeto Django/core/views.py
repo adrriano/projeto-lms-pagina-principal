44a3443bf81409b6ba89bd8a1402a640a5e8163c
Andrei Oliveira Souza Costa <andreituning@hotmail.com>
2018-10-30 00:43:38 -0300

    Link_funcional

--- Projeto Django/core/views.py
+++ Projeto Django/core/views.py
@@ -1,14 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def index(request):
-   
-   
     return render (request,"index.html")
 
 def curso_administracao(request):
-   
-   
     return render (request,"curso_administracao.html") 
-    
+
+def banco_de_dados(request):
+    return render (request,"banco_de_dados.html") 
+
+def sistemas_de_informacao(request):
+    return render (request," sistema_de_informacao.html")     
+
+def engenharia_da_computacao(request):
+    return render (request,"engenharia_da_computacao.html") 
+
+def gestao_da_tecnologia_da_informacao(request):
+    return render (request,"gestao_da_tecnologia_da_informacao.html") 
+
+def jogos_digitais(request):
+    return render (request,"jogos_digitais.html")
+
+def processos_gerenciais(request):
+    return render (request,"processos_gerenciais.html") 
+
+def producao_multimidia(request):
+    return render (request,"producao_multimidia.html") 
+
+def redes_de_computadores(request):
+    return render (request,"curso_administracao.html") 
--- Projeto Django/projeto/urls.py
+++ Projeto Django/projeto/urls.py
@@ -17,8 +17,17 @@ from django.contrib import admin
 from django.urls import path
 from core.views import *
 
+
 urlpatterns = [
     path('',index),
-    path('curso_administracao',curso_administracao),
     path('admin/', admin.site.urls),
+    path('curso_administracao.html',curso_administracao),
+    path('banco_de_dados.html',banco_de_dados),
+    path('sistemas_de_informacao.html',sistemas_de_informacao),
+    path('engenharia_da_computacao.html',engenharia_da_computacao),
+    path('gestao_da_tecnologia_da_informacao.html',gestao_da_tecnologia_da_informacao),
+    path('jogos_digitais.html',jogos_digitais),
+    path('processos_gerenciais.html',processos_gerenciais),
+    path('producao_multimidia.html',producao_multimidia),
+    path('redes_de_computadores.html',redes_de_computadores),
 ]
