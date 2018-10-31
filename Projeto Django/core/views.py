533a4351b51c57eb579bab3ba15b8e044d27d3ee
marianeplenck <marianeplenckauskas@gmail.com>
2018-10-31 11:08:16 -0300

    inclusão de Páginas WEB e Correção de ERRO

--- Projeto Django/core/views.py
+++ Projeto Django/core/views.py
@@ -12,26 +12,29 @@ def home(request):
 def curso_administracao(request):
     return render (request,"curso_administracao.html") 
 
-def banco_de_dados(request):
-    return render (request,"banco_de_dados.html") 
+def curso_banco_de_dados(request):
+    return render (request,"curso_banco_de_dados.html") 
 
-def sistemas_de_informacao(request):
-    return render (request," sistema_de_informacao.html")     
+def curso_sistemas_de_informacao(request):
+    return render (request," curso_sistema_de_informacao.html")     
 
-def engenharia_da_computacao(request):
-    return render (request,"engenharia_da_computacao.html") 
+def curso_engenharia_da_computacao(request):
+    return render (request,"curso_engenharia_da_computacao.html") 
 
-def gestao_da_tecnologia_da_informacao(request):
-    return render (request,"gestao_da_tecnologia_da_informacao.html") 
+def curso_gestao_da_tecnologia_da_informacao(request):
+    return render (request,"curso_gestao_da_tecnologia_da_informacao.html") 
 
-def jogos_digitais(request):
-    return render (request,"jogos_digitais.html")
+def curso_jogos_digitais(request):
+    return render (request,"curso_jogos_digitais.html")
 
-def processos_gerenciais(request):
-    return render (request,"processos_gerenciais.html") 
+def curso_processos_gerenciais(request):
+    return render (request,"curso_processos_gerenciais.html") 
 
 def curso_producao_multimidia(request):
     return render (request,"curso_producao_multimidia.html") 
 
 def curso_redes_de_computadores(request):
     return render (request,"curso_redes_de_computadores.html") 
+
+#def curso_analise_e_desenvolvimento_sistemas(request):
+#    return render (request,"curso_analise_e_desenvolvimento_de_sistemas.html" )
